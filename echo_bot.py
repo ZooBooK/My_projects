8171a3e09d8d689864089a253b9bd864315ca5c6
ZooBooK <56765885+ZooBooK@users.noreply.github.com>
2021-12-15 19:25:18 +0300

    Add files via upload
    My first echo bot! <3

---
+++ echo_bot.py
@@ -0,0 +1,14 @@
+import telebot
+
+bot = telebot.TeleBot("1951712553:AAGBDtMCAQQOnT_TCiZTvaXi-mN6eMh7-p4", parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+
+def send_welcome(message):
+    bot.reply_to(message, 'Howdy, how are you doing?')
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
